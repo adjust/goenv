3832b8e2ca15ed38ea954ba008c9ab4462fb5a94
Robert Abraham <robert@adeven.com>
2013-12-13 17:25:37 +0100

    change default redis db

--- redis.go
+++ redis.go
@@ -7,7 +7,7 @@ func (goenv *Goenv) GetRedis() (host, port string, db int) {
 func (goenv *Goenv) GetNamedRedis(name string) (host, port string, db int) {
 	host = goenv.Get(name+".host", "localhost")
 	port = goenv.Get(name+".port", "6379")
-	db = goenv.GetInt(name+".db", 0)
+	db = goenv.GetInt(name+".db", -1)
 
 	return
 }
