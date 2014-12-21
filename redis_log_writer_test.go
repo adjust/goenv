89c093c0c9c80a4a4bf009d24c52f7d958e65919
Paul H. Müller <paul@adeven.com>
2014-12-21 19:18:30 +0100

    Add tests for redis log writer

---
+++ redis_log_writer_test.go
@@ -0,0 +1,22 @@
+package goenv
+
+import (
+	"testing"
+)
+
+func TestRedisLogWriter(t *testing.T) {
+	goenv := NewGoenv("./test_config.yml", "redis", "nil")
+	writer, err := goenv.NewRedisLogWriter("test123")
+	if err != nil {
+		t.Fatalf("couldn't connect to redis for log writer test: %s", err)
+	}
+	writer.Write([]byte("dingdong"))
+	value, err := writer.redisClient.RPop("test123").Result()
+	if err != nil {
+		t.Errorf("error reading log writer: %s", err)
+	}
+
+	if value != "dingdong" {
+		t.Error("output didn't match input from log writer")
+	}
+}
