5416b0468f7f8c4d53332890d48841652a9d1a5e
Nitsan Shushan <nitsan.sh@fiverr.com>
2017-11-30 15:40:27 +0200

    [TEST] updated tests

--- goenv_template_test.go
+++ goenv_template_test.go
@@ -9,12 +9,14 @@ const (
 	REDIS_HOST = "my_redis_host"
 	REDIS_PORT = "1234"
 	ENV = "development"
+	LIST = "1 2 3"
 )
 
 func init() {
 	os.Setenv("REDIS_PORT", REDIS_PORT)
 	os.Setenv("REDIS_HOST", REDIS_HOST)
 	os.Setenv("DEV_ENV", ENV)
+	os.Setenv("LIST", LIST)
 }
 
 func TestNewTemplateGoenv(t *testing.T) {
@@ -26,5 +28,13 @@ func TestNewTemplateGoenv(t *testing.T) {
 
 	if port != REDIS_PORT {
 		t.Errorf("port != %s, got %s", REDIS_PORT, port)
+	}
+}
+
+func TestListTemplate(t *testing.T) {
+	goenv := NewTemplateGoenv("./test_template.yml", "", "nil")
+	arr := goenv.Get("list", "")
+	if arr != "[1 2 3]" {
+		t.Errorf("arr is %v", arr)
 	}
 }
